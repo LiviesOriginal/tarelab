c0a3bfc81765ea2a6ab71d8483292b15f6a567cb
LiviesOriginal <liviesoriginal@proton.me>
2026-09-21 02:59:17 -0400

    Update mug image source path

--- vites/falltoss/engine.ts
+++ vites/falltoss/engine.ts
@@ -38,7 +38,7 @@ export function createEngine(canvas: HTMLCanvasElement, cbs: EngineCallbacks): G
   mugImg.onerror = () => {
     mugReady = false;
   };
-  mugImg.src = '/vites/falltoss/public/mug.png';
+  mugImg.src = '/vites/falltoss/mug.png';
 
   const mug = { x: MUG.cx, phase: 0 };
 
