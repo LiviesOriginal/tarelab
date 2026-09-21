5c3d15f5cdf221fd8944057b7385c3bdae64fa4f
LiviesOriginal <liviesoriginal@proton.me>
2026-09-21 03:20:11 -0400

    Rename vites directory to ourcade

--- ourcade/falltoss/vite.config.ts
+++ ourcade/falltoss/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/vites/falltoss/",
+  base: "/ourcade/falltoss/",
   server: {
     port: 5173
   }
--- ourcade/galigo/vite.config.js
+++ ourcade/galigo/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/vites/galigo/',
+  base: '/ourcade/galigo/',
 })
