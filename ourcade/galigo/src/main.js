f4da93b020a0adec2763938dfd150f0b612db992
LiviesOriginal <liviesoriginal@proton.me>
2026-09-25 19:47:40 -0400

    Load game CSS from the HTML entry point

--- ourcade/galigo/src/main.js
+++ ourcade/galigo/src/main.js
@@ -1,6 +1,4 @@
 
-const css=document.createElement('link');css.rel='stylesheet';css.href='/ourcade/galigo/src/style.css';document.head.appendChild(css);
-
 const REPO='https://raw.githubusercontent.com/sunny567s35/Galaga_game/main/galaga/';
 const IMG={player:REPO+'images/player.png',enemy:REPO+'images/enemy-2.png',enemy2:REPO+'images/enemy-1.png',enemy3:REPO+'images/enemy.png',bullet:REPO+'images/missile2.png',enemyBullet:REPO+'images/missile1.png',stars:REPO+'images/stars.png',blast:REPO+'images/blast.gif'};
 const AUDIO={music:REPO+'audio/galaga.mp3',laser:REPO+'audio/audio_laser.ogg',hit:REPO+'audio/killenemy.mp3',enemyHit:REPO+'audio/audio_enemy-hit.ogg'};
