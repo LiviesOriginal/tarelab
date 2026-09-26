0ac50ecfeb4767f188807d1196305507fe4b8998
LiviesOriginal <liviesoriginal@proton.me>
2026-09-26 04:27:55 -0400

    Add files via upload

---
+++ ourcade/ourcade-image-build/games.js
@@ -0,0 +1,8 @@
+window.OURCADE_GAMES = [
+  {id:"galigaga",title:"GALIGAGA",subtitle:"Arcade action",href:"./galigaga/",accent:"#19a7ff",screen:"GALIGAGA",image:"./galigo.png"},
+  {id:"made4u",title:"MADE 4 U",subtitle:"Made just for you",href:"./made4u.html",accent:"#ff24d4",screen:"MADE 4 U",image:"./made4u.png"},
+  {id:"bubblepop",title:"BUBBLE POP",subtitle:"Pop your way through",href:"./bubblepop.html",accent:"#ff4f91",screen:"POP!",image:"./bubblepop.png"},
+  {id:"couplet",title:"COUPLET",subtitle:"A little game for two",href:"./couplet.html",accent:"#c78cff",screen:"COUPLET",image:"./couplet.png"},
+  {id:"oracle",title:"ORACLE",subtitle:"Ask the arcade",href:"./oracle.html",accent:"#54ff9b",screen:"ORACLE",image:"./oracle.png"},
+  {id:"falltoss",title:"FALL TOSS",subtitle:"Aim. Toss. Score.",href:"./falltoss/",accent:"#ffb52e",screen:"TOSS!",image:"./falltoss.png"}
+];
